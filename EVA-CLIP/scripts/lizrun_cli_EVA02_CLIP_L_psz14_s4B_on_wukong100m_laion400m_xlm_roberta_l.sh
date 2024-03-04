eae9b876c27485304220dee1a6afe3b9bbe4a551
root <root@cv-chenjiayu5-s13zff-0.cv-chenjiayu5-s13zff.lizr-a.svc.dev00-bcebj.local>
2024-03-04 09:26:46 +0800

    update

---
+++ EVA-CLIP/scripts/lizrun_cli_EVA02_CLIP_L_psz14_s4B_on_wukong100m_laion400m_xlm_roberta_l.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+lizrun start -c "/mnt/pfs-guan-ssai/cv/cjy/codebase/EVA/EVA-CLIP/scripts/lizrun_EVA02_CLIP_L_psz14_s4B_on_wukong100m_laion400m_xlm_roberta_l.sh" -n 8 -j eva02clipl-wkla400m-xlmrobertal-8node -i "reg-ai.chehejia.com/ssai/lizr/cu118/py310/pytorch:2.0.0-multinode-nlp" #-g 4
+
